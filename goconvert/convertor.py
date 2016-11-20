0860fec06499db317bc9aedf8084098a93a0f6c5
podhmo <ababjam61+github@gmail.com>
2016-11-21 01:02:35 +0900

    simple conversion functioin

---
+++ goconvert/convertor.py
@@ -0,0 +1,126 @@
+from .typeresolver import _wrap_value
+
+
+class Gensym(object):
+    def __init__(self, value="tmp"):
+        self.value = value
+        self.i = 0
+
+    def __call__(self):
+        self.i += 1
+        return "{}{}".format(self.value, self.i)
+
+
+class CoerceMap(object):
+    def __init__(self, resolver):
+        self.override_map = {}
+        self.resolver = resolver
+
+    def as_override(self, src_type, dst_type):
+        def decorator(on_write):
+            self.override(src_type, dst_type, on_write)
+            return on_write
+        return decorator
+
+    def override(self, src_type, dst_type, on_write):
+        src_type = _wrap_value(src_type)
+        dst_type = _wrap_value(dst_type)
+        self.resolver.add_relation(src_type, dst_type)
+        self.override_map[(src_type, dst_type)] = on_write
+
+    def __contains__(self, pair):
+        return pair in self.override_map
+
+    def __getitem__(self, pair):
+        return self.override_map[pair]
+
+    def get(self, pair):
+        return self.override_map.get(pair)
+
+
+class ConvertRequest(object):
+    def __init__(self, m, iw=None):
+        self.m = m
+        self.iw = iw
+
+    def new_request(self, m=None):
+        m = m or self.m
+        return self.__class__(m.submodule(newline=False), self.iw)
+
+
+class ConvertorFromMinicode(object):
+    def __init__(self, coerce_map):
+        self.coerce_map = coerce_map
+        self.gensym = Gensym()
+
+    def coerce(self, request, value, src_type, dst_type):
+        pair = (src_type, dst_type)
+        if pair in self.coerce_map:
+            return self.override_map[pair](self, request, value, src_type, dst_type)
+        if isinstance(dst_type, (list, tuple)):
+            r = []
+            for x in dst_type:
+                if x == "pointer":
+                    r.append("*")
+                elif x == "array":
+                    r.append("[]")
+                else:
+                    r.append(x.rsplit("/", 1)[-1])
+            return "{}({})".format("".join(r), value)
+        elif "/" in dst_type:
+            prefix, name = dst_type.rsplit(".")
+            if request.iw:
+                new_prefix = request.iw.import_fullname(prefix)  # writing import clause if needed
+            return "{}.{}({})".format(new_prefix, name, value)
+        else:
+            return "{}({})".format(dst_type, value)
+
+    def code_from_minicode(self, request, code, value):
+        is_cast = False
+        # optimization: x -> y; y -> z => z(x)
+        coerce_buf = []
+
+        def consume_buf(value):
+            if not coerce_buf:
+                return value
+            value = self.coerce(request, value, *coerce_buf[-1])
+            tmp = self.gensym()
+            request.m.stmt("{} := {}".format(tmp, value))
+            coerce_buf.clear()
+            return tmp
+
+        for i, op in enumerate(code):
+            if is_cast:
+                tmp = self.gensym()
+                request.m.stmt("{} := {}".format(tmp, value))
+                value = tmp
+                is_cast = False
+
+            if op[0] == "deref":
+                value = consume_buf(value)
+                with request.m.if_("{} != nil".format(value)):
+                    value = "*({})".format(value)
+                    tmp = self.gensym()
+                    request.m.stmt("{} := {}".format(tmp, value))
+                    return self.code_from_minicode(request.new_request(), code[i + 1:], tmp)
+            elif op[0] == "ref":
+                value = consume_buf(value)
+                value = "&({})".format(value)
+                is_cast = True
+            elif op[0] == "coerce":
+                pair = (op[1], op[2])
+                if pair not in self.coerce_map:
+                    coerce_buf.append((op[1], op[2]))
+                    continue
+                value = consume_buf(value)
+                value = self.coerce(request, value, op[1], op[2])
+                is_cast = True
+            # elif op[0] == "iterate":
+            #     value = consume_buf(value)
+            #     # todo: deep nested
+            #     value = self.iterate(m, value, op[1:], src_field, dst_field)
+            #     is_cast = True
+            else:
+                value = consume_buf(value)
+                raise NotImplementedError(op[0])
+        return request.m, consume_buf(value)
---
+++ goconvert/tests/test_convertor.py
@@ -0,0 +1,81 @@
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def _getTargetClass(self):
+        from goconvert.convertor import ConvertorFromMinicode
+        return ConvertorFromMinicode
+
+    def _makeOne(self, items):
+        from goconvert.typeresolver import TypeMappingResolver
+        from goconvert.convertor import CoerceMap
+        return self._getTargetClass()(CoerceMap(TypeMappingResolver(items)))
+
+    def _makeRequest(self):
+        from goconvert.convertor import ConvertRequest
+        from prestring.go import GoModule
+        return ConvertRequest(GoModule())
+
+    def test_convert_ref(self):
+        target = self._makeOne([])
+        request = self._makeRequest()
+        value = "v"
+        code = [("ref",)]
+        _, value = target.code_from_minicode(request, code, value)
+        self.assertEqual("", str(request.m))
+        self.assertEqual("&(v)", value)
+
+    def test_convert_deref(self):
+        target = self._makeOne([])
+        request = self._makeRequest()
+        value = "v"
+        code = [("deref",)]
+        _, value = target.code_from_minicode(request, code, value)
+        expected = """\
+if v != nil  {
+\ttmp1 := *(v)
+}\
+"""
+        self.assertEqual(expected, str(request.m))
+        self.assertEqual("tmp1", value)
+
+    def test_convert_coerce(self):
+        from goconvert.typeresolver import Action
+        target = self._makeOne([("string", "X")])
+        request = self._makeRequest()
+        value = "v"
+        code = [Action(action="coerce", src=("string",), dst=("X", ))]
+        _, value = target.code_from_minicode(request, code, value)
+        self.assertEqual("tmp1 := X(v)", str(request.m))
+        self.assertEqual("tmp1", value)
+
+    def test_convert_coerce_ref(self):
+        from goconvert.typeresolver import Action
+        target = self._makeOne([("string", "X")])
+        request = self._makeRequest()
+        value = "v"
+        code = [("ref",), Action(action="coerce", src=("string",), dst=("X", ))]
+        _, value = target.code_from_minicode(request, code, value)
+        expected = """\
+tmp1 := &(v)
+tmp2 := X(tmp1)\
+"""
+
+        self.assertEqual(expected, str(request.m))
+        self.assertEqual("tmp2", value)
+
+    def test_convert_deref_coerce(self):
+        from goconvert.typeresolver import Action
+        target = self._makeOne([("string", "X")])
+        request = self._makeRequest()
+        value = "v"
+        code = [("deref",), Action(action="coerce", src=("string",), dst=("X", ))]
+        _, value = target.code_from_minicode(request, code, value)
+        expected = """\
+if v != nil  {
+\ttmp1 := *(v)
+\ttmp2 := X(tmp1)
+}\
+"""
+        self.assertEqual(expected, str(request.m))
+        self.assertEqual("tmp2", value)
